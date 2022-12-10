a0e22c7b3067864d1ba33b64b8ecb9e7689c40cc
Muxriddin314 <111342863+Muxriddin314@users.noreply.github.com>
2022-12-10 19:21:54 +0500

    Update 3-masala.js

--- 3-masala.js
+++ 3-masala.js
@@ -1,12 +1,12 @@
 
 function isLeapYear(year) {
     let result = null;
-    if(year % 4 == 0 ){
-      result = true
-    }
-
-    else if(year % 400 != 0) {
+    
+    if(year % 400 != 0) {
       result = false
+    }
+    else if(year % 4 == 0 ){
+      result = true
     }
     else {
       result = false
